572fab2c6c0444f33265b3057ea115223ee12368
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-12-02 00:42:53 +0100

    add missing file

--- artifact/scripts/env_pack.sh
+++ artifact/scripts/env_pack.sh
@@ -2,3 +2,5 @@
 mkdir ../package
 cd ../airly/lib/python3.6/site-packages/
 zip -r ../../../../package/AirlyAlexaMainFunction.zip .
+cd ../../../..
+zip package/AirlyAlexaMainFunction.zip AirlyAlexaMainFunction.py
