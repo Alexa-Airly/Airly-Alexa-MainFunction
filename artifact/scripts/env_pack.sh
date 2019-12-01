85cd3bd3dac5f5633c83aa113fd5ea54c01742d7
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-12-02 00:49:49 +0100

    fix

--- artifact/scripts/env_pack.sh
+++ artifact/scripts/env_pack.sh
@@ -3,4 +3,4 @@ mkdir ../package
 cd ../airly/lib/python3.6/site-packages/
 zip -r ../../../../package/AirlyAlexaMainFunction.zip .
 cd ../../../..
-zip package/AirlyAlexaMainFunction.zip AirlyAlexaMainFunction.py
+zip package/AirlyAlexaMainFunction.zip ../py/AirlyAlexaMainFunction.py
